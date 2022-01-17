b66a7989d5fde925459cb86ccd613f710ef09340
jaejun1130 <wjswowns1234@gmail.com>
2022-01-17 21:24:19 +0900

    Bean callBack

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,46 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * InitializingBean
+ * DisposableBean
+ */
+public class NetworkClient {
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출 url = "+url);
+    }
+
+    public void connect() {
+        System.out.println("connect url = " + url);
+    }
+
+    public void call(String message){
+        System.out.println("call message = " + message);
+    }
+
+    public void disconnect(){
+        System.out.println("disconnect url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void init()  {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void close()  {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+}
