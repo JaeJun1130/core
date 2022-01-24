efd9af9c48033c4d3d471d811cae88d4e37bf334
jaejun1130 <wjswowns1234@gmail.com>
2022-01-24 21:21:09 +0900

    Provider, request, proxyMode

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,12 +2,14 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWithPrototypeTest1 {
 
@@ -32,19 +34,16 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean bean2 = ac.getBean(ClientBean.class);
         int logic1 = bean2.logic();
-        Assertions.assertThat(logic1).isEqualTo(2);
+        Assertions.assertThat(logic1).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private  final  PrototypeBean prototypeBean; //싱글톤 객체에서 주입받을 때 이미 값이 할당되서 들고있음.
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeansProvider;
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeansProvider.get();
             prototypeBean.addCount();;
             return prototypeBean.getCount();
         }
